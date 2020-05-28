cd8f75d86cac880cda45eafa47bf780cf8229e54
Jon <hadnet@users.noreply.github.com>
2020-05-28 17:15:36 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  mode: 'development',
+  entry: {
+    app: [
+      './src/app.tsx'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: (m) => { return /\.(ts|tsx|js|jsx)$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: (m) => { return /\.css$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) },
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: (m) => { return /\.(png|jp(e*)g|svg)$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) },
+        use: [{
+          loader: 'url-loader',
+          options: { 
+            limit: 8000,
+            name: 'images/[hash]-[name].[ext]'
+          } 
+        }]
+      }
+    ]
+  },
+  plugins: [],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  }
+};
