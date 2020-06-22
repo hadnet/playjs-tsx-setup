c2173dd539003bcf4300777c05f4b5973a1e0ac3
Jon <hadnet@users.noreply.github.com>
2020-06-21 23:16:18 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,7 @@
-/// <reference path='./png.d.ts' />
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app.css';
-import logo from '../public/logo.png'; 
+import GlobalStyle from './globalStyle';
+import Logo from './logo'
 
 type P = {}
 type S = {}
@@ -10,24 +9,11 @@ type S = {}
 class App extends React.PureComponent<P,S> {
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/app.jsx</code> and tap.... on the play button.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <GlobalStyle dark />
+      <Logo />
+      </>
     )
   }
 }
-
 ReactDOM.render(<App />, document.getElementById('root'));
