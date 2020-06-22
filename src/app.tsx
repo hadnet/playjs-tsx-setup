3f156045ac19d3e69c4f198592c9b97fea7d1d2e
Jon <hadnet@users.noreply.github.com>
2020-06-21 23:21:03 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GlobalStyle from './globalStyle';
+import GlobalStyle from './GlobalStyle';
 import Logo from './Logo'
 
 type P = {}
