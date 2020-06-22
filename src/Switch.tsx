82c491d2990f57de1ace7e421ff467dea9314cc5
Jon <hadnet@users.noreply.github.com>
2020-06-22 05:06:45 -0300

    Update Switch.tsx

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Switch from 'react-switch';
-import {IoIosSunny, IoIosMoon} from 'react-icons/io'
+import {IoIosSunny, IoIosMoon} from 'react-icons/io';
 
 type P = {
   onChange: () => void;
